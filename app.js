a77b292a010c983ccebfca573d37c4f9f41547ce
CcasperLiu <lzj40404@gmail.com>
2020-01-13 11:50:54 -0800

    added route get:comments/new and post:comments

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get("/campgrounds",function(req,res){
   Campground.find({},function(err,allCampgrounds){
     if(err) console.log(err);
     else{
-      res.render("index",{campgrounds:allCampgrounds});
+      res.render("campgrounds/index",{campgrounds:allCampgrounds});
     }
   })
 });
@@ -48,7 +48,7 @@ app.post("/campgrounds",function(req, res){
 
 app.get("/campgrounds/new", function(req,res){
 
-   res.render("new.ejs");
+   res.render("campground/new");
 });
 
 app.get("/campgrounds/:id",function(req,res){
@@ -57,12 +57,45 @@ app.get("/campgrounds/:id",function(req,res){
     if(err) console.log(err);
     else{
       console.log(foundCampground);
-      res.render("show",{campground:foundCampground});
+      res.render("campgrounds/show",{campground:foundCampground});
     }
   });
 })
 
-app.listen(3000, function(){
+
+// ----------Comment Routes
+app.get("/campgrounds/:id/comments/new", function(req,res){
+  //find campground by id
+  Campground.findById(req.params.id,function(err,campground){
+    if(err) console.log(err);
+    else {
+      res.render("comments/new", {campground:campground});
+    }
+  })
+});
+
+app.post("/campgrounds/:id/comments", function(req,res){
+  Campground.findById(req.params.id,function(err,campground){
+    if(err) {
+      console.log(err);
+      redirect("/campgrounds");
+    }
+    else{
+
+      Comment.create(req.body.comment, function(err, comment){
+        if (err) console.log(err);
+        else{
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/"+campground._id);
+        }
+      })
+
+    }
+  })
+})
+
+app.listen(8080, function(){
 	console.log("yelpCamp server has started!");
 });
 
